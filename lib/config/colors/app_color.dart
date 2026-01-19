daa8a1a2c63d09800a5ef216c21c5135fea00a35
Chorus Rajbanshi <chorusraj129@gmail.com>
2026-01-19 16:39:49 +0545

    Clean architecture

---
+++ lib/config/colors/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Color(0xff6200ee);
+  static const Color secondaryColor = Color(0xff03dac6);
+}
---
+++ lib/core/constants/api_const.dart
@@ -0,0 +1,6 @@
+class ApiConst {
+ static const String baseurl="https://fakestoreapi.com";
+ static const String productsEndpoint ="/products";
+ static const String refreshToken ="/auth/refresh";
+ 
+}
